675cab358b6363c14812d24f41557fe2779ac631
Gupta <agupta77@deloitte.com>
2024-08-30 12:16:22 +0530

    KPIs Added in Table

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import os
 from PIL import Image
+import pandas as pd
 
 # Define the base directory for the graphs
 BASE_DIR = "State"
@@ -20,14 +21,14 @@ selected_year = st.sidebar.selectbox("Select Financial Year", financial_years)
 # Dictionary to store KPI statistics for each financial year
 kpi_stats = {
     "2022-23": {
-        "Number of duplicate records": 47864,
-        "Number of unique duplicate records": 20345,
-        "Total Amounts of unique duplicate records": 412797710.29
+        "Number of Duplicate Transactions": 47864,
+        "Number of Unique Duplicate Transactions": 20345,
+        "Total Revenue Due to Duplicate Transactions": 412797710.29
     },
     "2023-24": {
-        "Number of duplicate records": 26766,
-        "Number of unique duplicate records": 11597,
-        "Total Amounts of unique duplicate records": 290331826.61
+        "Number of Duplicate Transactions": 26766,
+        "Number of Unique Duplicate Transactions": 11597,
+        "Total Revenue Due to Duplicate Transactions": 290331826.61
     }
 }
 
@@ -35,9 +36,20 @@ kpi_stats = {
 st.write(f"## Key Performance Indicators for {selected_state} - {selected_year}")
 
 if selected_year in kpi_stats:
-    st.write(f"- **Number of duplicate records**: {kpi_stats[selected_year]['Number of duplicate records']:,}")
-    st.write(f"- **Number of unique duplicate records**: {kpi_stats[selected_year]['Number of unique duplicate records']:,}")
-    st.write(f"- **Total Amounts of unique duplicate records**: ₹{kpi_stats[selected_year]['Total Amounts of unique duplicate records']:,.2f}")
+    kpi_data = {
+        "Description": [
+            "Number of Duplicate Transactions",
+            "Number of Unique Duplicate Transactions",
+            "Total Revenue Due to Duplicate Transactions"
+        ],
+        "Figures": [
+            f"{kpi_stats[selected_year]['Number of Duplicate Transactions']:,}",
+            f"{kpi_stats[selected_year]['Number of Unique Duplicate Transactions']:,}",
+            f"₹{kpi_stats[selected_year]['Total Revenue Due to Duplicate Transactions'] / 10**7:,.2f} Crore"
+        ]
+    }
+    kpi_df = pd.DataFrame(kpi_data)
+    st.table(kpi_df)
 else:
     st.write("No KPI data available for the selected year.")
 
